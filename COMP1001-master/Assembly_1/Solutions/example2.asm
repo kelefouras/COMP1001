c1dc3983758e540b75e3bc84a42d5eec5f3d5a70
Kelefouras Vasilis <kelefourasvasilis@gmail.com>
2022-10-05 17:13:16 +0100

    fixed_bug

--- COMP1001-master/Assembly_1/Solutions/example2.asm
+++ COMP1001-master/Assembly_1/Solutions/example2.asm
@@ -6,7 +6,7 @@ ExitProcess PROTO, dwExitCode: DWORD  ; Exit process prototype
 
 .data ; data segment
 	; define your variables here
-answer DWORD ?; Reserve 1 DWORD for the solution
+answer SDWORD ?; Reserve 32 bits for the output. The output can be negative, thus a signed data type is needed.
 
 
 .code ; code segment
@@ -14,12 +14,12 @@ answer DWORD ?; Reserve 1 DWORD for the solution
 main PROC ; main procedure
 	; write your assembly code here
 
-mov eax, 1 		; Value for A stored in eax
+mov eax, 10 		; Value for A stored in eax
 mov ebx, 2 		; Value for B stored in ebx
 mov ecx, 3 		; Value for C stored in ecx
 mov edx, 4 		; Value for D stored in edx
 
-add eax, ebx 		; Add 1 + 2 and store in eax
+add eax, ebx 		; Add 10 + 2 and store in eax
 add ecx, edx 		; Add 3 + 4 and store in ecx
 
 sub eax, ecx 		; Subtract ecx from eax and store in eax
@@ -30,3 +30,10 @@ mov answer, eax 	; Move value from eax into variable
   
 main ENDP ; exit main procedure
 END main  ; stop assembling
+
+
+
+; In VS, under the registers tab you can see the following flag registers:
+; Overflow flag(OV): Set to 1, when given instruction is for e.g of 32bit and resultant value is of 33bit.
+; Sign(PL): Set to 1, when negative result
+; Zero(ZR): set to 1, if result is zero 
